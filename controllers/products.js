e9cab806b90e89d5952403455113089c6e5462c6
Subha Ghanta <subhaghanta60@gmail.com>
2024-07-19 17:31:16 +0530

    Advanced Search Funcanility

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,17 @@
 const Product = require("../model/products");
 const getAllproducts = async(req,res) => {
 
-    const { company } =req.query;
+    const { company,name,featured } =req.query;
     const queryObject ={};
 
     if(company){
-        queryObject.company= company;
-       
+        queryObject.company= { $regex:company, $options: "i"};
+    }
+    if(name){
+        queryObject.name= { $regex:name, $options: "i"};
+    }
+    if(featured){
+        queryObject.name= { $regex:featured, $options: "i"};
     }
 
     const mydata= await Product.find(queryObject);
